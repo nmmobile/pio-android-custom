f8258ed0e296214c609b17211f528940cedf242c
innovativecs12 <password18>
2015-01-23 11:20:17 -0500

    test

--- PushIO Demo Android App (Basic)/gen/com/pushio/basic/BuildConfig.java
+++
@@ -1,8 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-/** Automatically generated file. DO NOT MODIFY */
-package com.pushio.basic;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
